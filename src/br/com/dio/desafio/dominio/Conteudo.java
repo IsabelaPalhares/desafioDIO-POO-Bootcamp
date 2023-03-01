156f9db1ba358c82b9a3985b72964817499cbbbd
isabelapalhares <isabelapalhareslage@hotmail.com>
2023-03-01 19:17:05 -0300

    polimorfismo

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -5,6 +5,7 @@ public abstract class Conteudo {
     protected static final double XP_PADRAO = 10d;
 
     private String titulo;
+
     private String descricao;
 
     public abstract double calcularXp();
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -7,7 +7,9 @@ import java.util.Set;
 public class Dev {
 
     private String nome;
+
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+
     private Set<Conteudo> conteudosConsluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
